b6e49a155943d0375a483508619a9635c5eef411
panjiasmoroart <pandjiasmoroart@gmail.com>
2019-08-22 13:02:22 +0700

    Updates

---
+++ precache-manifest.72559041687a68c5f0572b00db715067.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "823a347090aa23e9933a3577450af466",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "92d4132ec8e0e98b2ee6",
+    "url": "/portfolio/static/css/2.0e081ea1.chunk.css"
+  },
+  {
+    "revision": "b5ec02280c5531367029",
+    "url": "/portfolio/static/css/main.3537c11d.chunk.css"
+  },
+  {
+    "revision": "92d4132ec8e0e98b2ee6",
+    "url": "/portfolio/static/js/2.b58b972b.chunk.js"
+  },
+  {
+    "revision": "b5ec02280c5531367029",
+    "url": "/portfolio/static/js/main.42c3d2de.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
