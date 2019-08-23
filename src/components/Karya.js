ec3f6509e9d26ff3b42f197e8665ee485ff1025e
panjiasmoroart <pandjiasmoroart@gmail.com>
2019-08-23 17:00:35 +0700

    update images in karya.js new

--- src/components/Karya.js
+++ src/components/Karya.js
@@ -13,11 +13,11 @@ class Karya extends Component {
 
             <div className="projects-grid">
               {/* Project 1 */}
-              <Card shadow={5} style={{width: '256px', height: '380px', background: 'url(https://i.ibb.co/JzR0G6T/PT-Multi-Sarana-Equipment-2018-05-21-15-44-31.png) center / cover', margin: 'auto'}}>
+              <Card shadow={5} style={{width: '256px', height: '380px', background: 'url(https://i.ibb.co/WxpNRNb/qlay-new.jpg) center / cover', margin: 'auto'}}>
               </Card>
 
               {/* Project 2 */}
-              <Card shadow={5} style={{width: '256px', height: '380px', background: 'url(https://i.ibb.co/r5WvSQv/PT-Qlay-Bangun-Jaya-Developer-Property-amp-Infrastruktur-Terbaik-Di-Depok-2019-04-24-09-03-23.png) center / cover', margin: 'auto'}}>
+              <Card shadow={5} style={{width: '256px', height: '380px', background: 'url(https://i.ibb.co/8z1V85g/new-multisarana.jpg) center / cover', margin: 'auto'}}>
               </Card>
 
               {/* Project 3 */}
